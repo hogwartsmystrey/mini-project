32ea5fac3b5c15aee4692d91011c4986336f4942
vimalcj <vimalchandran.cj@gmail.com>
2019-11-23 18:44:42 +0530

    Test Commit

---
+++ src/video/rental/software/config/Db_test.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video.rental.software.config;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author vimal
+ */
+public class Db_test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+        // for (int i = 0; i < 100; i++) {
+        Connection connection = SqlConnection.getConnection();
+        String query = "SELECT * FROM customer";
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareStatement(query);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Long customerId = resultSet.getLong("customer_id");
+                String mobileNumber = resultSet.getString("mobile_number");
+                System.out.println("Customer ID " + customerId + " AND Mobile Number is " + mobileNumber);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Db_test.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            SqlConnection.closeConnection();
+        }
+    }
+    //}
+
+}
---
+++ src/video/rental/software/config/SqlConnection.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video.rental.software.config;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author vimal
+ */
+public class SqlConnection {
+
+    static final Logger logger = Logger.getLogger(SqlConnection.class.getName());
+    private static Connection connection;
+
+    public SqlConnection() throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+    }
+
+    public static Connection getConnection() throws SQLException {
+        logger.log(Level.INFO, "Inside acquring connection");
+        if (null == connection) {
+            connection = (Connection) DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306/video_rental",
+                            "root", "admin@12345");
+
+        }
+        logger.log(Level.INFO, "Returning connection{0}", connection);
+        return connection;
+    }
+
+    public static void closeConnection() throws SQLException {
+        logger.log(Level.INFO, "Closing connection{0}", connection);
+        if (null != connection) {
+            connection.close();
+        }
+    }
+
+}
